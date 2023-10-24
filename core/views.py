43e99f09ce1b50720950472b5cad335e61e80d4b
Swantje Moehle <s.moehle@alumni.ubc.ca>
2023-10-23 22:56:13 -0700

    added plant info template and styling

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Plant_info, Seed_inventory
@@ -13,9 +13,20 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
+# def detail(request, plant_info_id):
+#    # output = "\n".join
+#    plant_info = Plant_info.objects.filter(id=plant_info_id)
+#    template = loader.get_template("core/plantinfo.html")
+#    context = {"plant_info": plant_info}
+#    return HttpResponse(template.render(context, request))
+# return HttpResponse("you're looking at plant variety %s" % plant_info_id)
+
+
 def detail(request, plant_info_id):
-    # output = "\n".join
-    return HttpResponse("you're looking at plant variety %s" % plant_info_id)
+    plant_info = get_object_or_404(Plant_info, id=plant_info_id)
+    variety_list = Seed_inventory.objects.filter(plant_name_id=plant_info_id)
+    context = {"plant_info": plant_info, "variety_list": variety_list}
+    return render(request, "core/plantinfo.html", context)
 
 
 def varieties(request, plant_info_id):
